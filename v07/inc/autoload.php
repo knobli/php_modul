4f465f3ae95632b3fe53e0fd123e077e9a74b0d8
knobli <raffael@santschi.ch>
2014-11-03 11:36:27 +0100

    add timezone

--- v07/inc/autoload.php
+++ v07/inc/autoload.php
@@ -1,5 +1,6 @@
 <?php 
-
+	date_default_timezone_set('Europe/Zurich');
+	
 	$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB,DB_USER,DB_PW);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
